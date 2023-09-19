9822f3346dbcf6ce4ee2cf0bb5997c35ec6abcf3
vladislav <ve@incsub.com>
2023-09-19 04:53:13 +0200

    Add first least significant byte detection

--- src/main.rs
+++ src/main.rs
@@ -94,16 +94,6 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn sign_extend(v: u32, width: u32) -> i32 {
-    assert!(width < 32);
-    let base: i32 = 2;
-    let mut res = v as i32;
-    if v as i32 > base.pow(width - 1) {
-        res = v as i32 - base.pow(width);
-    }
-    res
-}
-
 #[derive(Default, Debug)]
 struct Cpu {
     register: Registers,
@@ -714,8 +704,18 @@ impl TryFrom<u32> for Register {
     }
 }
 
+fn sign_extend(v: u32, width: u32) -> i32 {
+    assert!(width < 32);
+    let base: i32 = 2;
+    let mut res = v as i32;
+    if v as i32 > base.pow(width - 1) {
+        res = v as i32 - base.pow(width);
+    }
+    res
+}
+
 #[cfg(test)]
-mod test {
+mod test_sign_extend {
     use super::*;
 
     #[test]
@@ -747,5 +747,47 @@ mod test {
         assert_eq!(sign_extend(neg as u32, 16), neg);
 
         assert_eq!(sign_extend(neg as u32, 16) as i16, neg as i16);
+    }
+}
+
+fn first_lsb_set(source: u32) -> i32 {
+    for x in 0..32 {
+        if source & (1 << x) > 0 {
+            return x;
+        }
+    }
+
+    return -1;
+}
+
+#[cfg(test)]
+mod test_first_lsb_set {
+    use super::*;
+
+    #[test]
+    fn lsb() {
+        let test = 0b0000_0000_0000_0000_0000_0000_0000_0000;
+        let result = first_lsb_set(test);
+        assert_eq!(result, -1);
+
+        let test = 0b0000_0000_0000_0000_0000_0000_0000_0001;
+        let result = first_lsb_set(test);
+        assert_eq!(result, 0);
+
+        let test = 0b0000_0000_0000_0000_0000_0000_0000_0010;
+        let result = first_lsb_set(test);
+        assert_eq!(result, 1);
+
+        let test = 0b0000_0000_0000_0000_0000_0000_0000_0100;
+        let result = first_lsb_set(test);
+        assert_eq!(result, 2);
+
+        let test = 0b0000_0000_0000_0000_0000_0000_0000_1000;
+        let result = first_lsb_set(test);
+        assert_eq!(result, 3);
+
+        let test = 0b1000_0000_0000_0000_0000_0000_0000_1000;
+        let result = first_lsb_set(test);
+        assert_eq!(result, 3);
     }
 }
